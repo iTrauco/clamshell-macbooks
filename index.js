953f30a603dc78762b6e2c703fc89e707f3e667a
Christopher Trauco <ctrauco@CTRAUCO.local>
2019-04-02 13:30:43 -0400

    app structure and index.js added

---
+++ index.js
@@ -0,0 +1,12 @@
+// I need express
+const express = require('express');
+// I need express to have its own port
+const PORT = 3000;
+// I need to create an app with express
+const app = express();
+// I need to connect a router
+app.listen(PORT, () => {
+    console.log(`Running on ${PORT}.`);
+});
+// I need express to listen on that port
+
